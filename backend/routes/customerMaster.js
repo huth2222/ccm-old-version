5ddecf3d5ebc5f33ec800b0eea8bee7d05d7c466
huth2222 <udon035it@gmail.com>
2025-11-13 12:14:29 +0700

    new

--- backend/routes/customerMaster.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const customerMasterController = require("../controllers/customerMasterController");
-
-const app = express();
-
-app.route("/").post(customerMasterController.index);
-
-module.exports = app;
--- backend/routes/forgotPassword.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const forgotPasswordController = require('../controllers/forgotPasswordController');
-
-const app = express();
-
-app.route('/')
-    .post( forgotPasswordController.forgotPassword);
-
-module.exports = app;
